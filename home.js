df151f4525a01db04d3bc53b48f7181514487cb2
Zidi Ebi <zididekeebi@gmail.com>
2022-10-29 00:34:28 +0200

    first commit

---
+++ home.js
@@ -0,0 +1,15 @@
+
+        // document.getElementById("C2A1").addEventListener("click", feedback);
+        // function feedback() {
+        //     var x = document.getElementById("About-Me");
+        //     if (x.style.display === "none") {
+        //         x.style.display = "flex";
+        //     } else {
+        //         x.style.display = "none";
+        //     }
+
+        //     let y = document.getElementById("pre-section");
+        //     if (x.style.display === "flex"){
+        //         y.style.display = "none";
+        //     }    
+        // }
